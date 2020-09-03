bb8d598991d70e8379374ef9d137018a29872ef1
programming-with-sakib <programmingwithsakib@gmail.com>
2020-09-03 19:19:22 +0600

    #Update MonsterFinder

--- 01 - MonsterFinder/MonsterFinder-final/index.js
+++ 01 - MonsterFinder/MonsterFinder-final/index.js
@@ -1,75 +1,80 @@
 import { monsters } from './monsters.js';
-console.log(monsters);
 
-showMonsters(monsters);
-notFound();
-
-function showMonsters(monsters) {
-  for (let monster of monsters) {
-    // image
-    const monsterImage = document.createElement('img');
-    monsterImage.src = `https://robohash.org/${monster.id}?set=set2`;
-    monsterImage.alt = monster.name;
-
-    // name
-    const monsterName = document.createElement('p');
-    monsterName.className = 'name';
-    monsterName.innerText = monster.name;
-
-    // email
-    const monsterEmail = document.createElement('p');
-    monsterEmail.className = 'email';
-    monsterEmail.innerText = monster.email;
-
-    // parent node
-    const parent = document.createElement('div');
-    parent.className = 'monster';
-    parent.append(monsterImage, monsterName, monsterEmail);
-
-    document.querySelector('.monsters').append(parent);
-  }
+for (let monster of monsters) {
+  showMonsters(monster);
 }
 
+function showMonsters(monster) {
+  const monsterDiv = document.createElement('div');
+  monsterDiv.className = 'monster';
+
+  // image
+  const img = document.createElement('img');
+  img.src = `https://robohash.org/${monster.id}?set=set2`;
+  img.alt = monster.name;
+
+  // name
+  const name = document.createElement('p');
+  name.className = 'name';
+  name.innerText = monster.name;
+
+  // email
+  const email = document.createElement('p');
+  email.className = 'email';
+  email.innerText = monster.email;
+
+  monsterDiv.append(img, name, email);
+
+  document.querySelector('.monsters').append(monsterDiv);
+}
+
+notFound();
+
 function notFound() {
-  const error = document.createElement('div');
-  error.className = 'p-5 not-found';
-  error.style.display = 'none';
+  const notFoundDiv = document.createElement('div');
+  notFoundDiv.className = 'p-5 not-found';
+  notFoundDiv.style.display = 'none';
 
   const span = document.createElement('span');
   span.innerText = '404';
 
   const h1 = document.createElement('h1');
   h1.innerText = '🧟‍♂️ No Monster Found 🧟‍♂️';
-  error.append(span, h1);
 
-  document.querySelector('.monsters').append(error);
+  notFoundDiv.append(span, h1);
+  document.querySelector('.monsters').append(notFoundDiv);
 }
 
 document
   .querySelector('#search-monster')
-  .addEventListener('keyup', searchMonster);
+  .addEventListener('keyup', function (e) {
+    const keyword = e.target.value.toLowerCase();
+    const monsters = document.querySelectorAll('.monster');
 
-function searchMonster(e) {
-  const keyword = e.target.value.toLowerCase();
-  const monsters = document.querySelectorAll('.monster');
+    let notFound = true;
 
-  let foundOne = false;
+    for (let monster of monsters) {
+      const name = monster.children[1].innerText.toLowerCase();
 
-  monsters.forEach((monster) => {
-    const name = monster.children[1].innerText.toLowerCase();
-    const email = monster.children[2].innerText.toLowerCase();
-    if (name.includes(keyword) || email.includes(keyword)) {
-      monster.style.display = 'block';
-      foundOne = true;
+      const email = monster.children[2].innerText.toLowerCase();
+
+      if (name.includes(keyword) || email.includes(keyword)) {
+        monster.style.display = 'block';
+        notFound = false;
+      } else {
+        monster.style.display = 'none';
+      }
+    }
+
+    if (notFound) {
+      document.querySelector('.not-found').style.display = 'block';
     } else {
-      monster.style.display = 'none';
+      document.querySelector('.not-found').style.display = 'none';
     }
   });
 
-  let notFound = document.querySelector('.not-found');
-  notFound.style.display = foundOne === true ? 'none' : 'block';
-}
-
 document
   .querySelector('#search-monster-form')
-  .addEventListener('submit', (e) => e.preventDefault());
+  .addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
--- 01 - MonsterFinder/MonsterFinder-starter/monsters.js
+++ 01 - MonsterFinder/MonsterFinder-starter/monsters.js
@@ -1,8 +1,24 @@
 const monsters = [
-  { id: 1, name: 'Sage Rutherford', email: 'Clay.Batz43@yahoo.com' },
-  { id: 2, name: 'Grace Nitzsche', email: 'Josiane.Emard17@yahoo.com' },
-  { id: 3, name: 'Casimer White', email: 'Jerad13@hotmail.com' },
-  { id: 4, name: 'Aidan Corkery', email: 'Jailyn96@hotmail.com' },
+  {
+    id: 1,
+    name: 'Sage Rutherford',
+    email: 'Clay.Batz43@yahoo.com',
+  },
+  {
+    id: 2,
+    name: 'Grace Nitzsche',
+    email: 'Josiane.Emard17@yahoo.com',
+  },
+  {
+    id: 3,
+    name: 'Casimer White',
+    email: 'Jerad13@hotmail.com',
+  },
+  {
+    id: 4,
+    name: 'Aidan Corkery',
+    email: 'Jailyn96@hotmail.com',
+  },
   {
     id: 5,
     name: 'Sakib Bailey',
@@ -23,8 +39,16 @@ const monsters = [
     name: 'Kendra Zboncak',
     email: 'Leila_Schuster@hotmail.com',
   },
-  { id: 9, name: 'Alford Runolfsdottir', email: 'Ivy27@hotmail.com' },
-  { id: 10, name: 'Delta Kozey', email: 'Aubrey.Okuneva@hotmail.com' },
+  {
+    id: 9,
+    name: 'Alford Runolfsdottir',
+    email: 'Ivy27@hotmail.com',
+  },
+  {
+    id: 10,
+    name: 'Delta Kozey',
+    email: 'Aubrey.Okuneva@hotmail.com',
+  },
   {
     id: 11,
     name: 'Deron Bernhard',
@@ -40,8 +64,16 @@ const monsters = [
     name: 'Linwood Bahringer',
     email: 'Salma.Padberg96@hotmail.com',
   },
-  { id: 14, name: 'Steve Waters', email: 'Mariam.Collier70@gmail.com' },
-  { id: 15, name: 'Dewitt Boyer', email: 'Marlene34@yahoo.com' },
+  {
+    id: 14,
+    name: 'Steve Waters',
+    email: 'Mariam.Collier70@gmail.com',
+  },
+  {
+    id: 15,
+    name: 'Dewitt Boyer',
+    email: 'Marlene34@yahoo.com',
+  },
   {
     id: 16,
     name: 'Sakib Leannon',
@@ -52,28 +84,56 @@ const monsters = [
     name: "Leonardo O'Keefe V",
     email: 'Sim.FlatSakiey74@hotmail.com',
   },
-  { id: 18, name: 'Ms. Michale Homenick', email: 'Janice88@gmail.com' },
-  { id: 19, name: 'Ms. John Borer', email: 'Dudley_Sakii@gmail.com' },
-  { id: 20, name: 'Kraig Schowalter', email: 'Saki@yahoo.com' },
-  { id: 21, name: 'Kari Green', email: 'sakib112@yahoo.com' },
+  {
+    id: 18,
+    name: 'Ms. Michale Homenick',
+    email: 'Janice88@gmail.com',
+  },
+  {
+    id: 19,
+    name: 'Ms. John Borer',
+    email: 'Dudley_Sakii@gmail.com',
+  },
+  {
+    id: 20,
+    name: 'Kraig Schowalter',
+    email: 'Saki@yahoo.com',
+  },
+  {
+    id: 21,
+    name: 'Kari Green',
+    email: 'sakib112@yahoo.com',
+  },
   {
     id: 22,
     name: 'Ms. Brooks Stehr',
     email: 'Abagail_Effertz90@hotmail.com',
   },
-  { id: 23, name: 'Sylvester Rohan', email: 'Keeley70@gmail.com' },
+  {
+    id: 23,
+    name: 'Sylvester Rohan',
+    email: 'Keeley70@gmail.com',
+  },
   {
     id: 24,
     name: 'Mr. Lonie Wintheiser',
     email: 'Cheyanne13Sak@yahoo.com',
   },
-  { id: 25, name: 'Shyann Hane', email: 'Kim_Murphy@gmail.com' },
+  {
+    id: 25,
+    name: 'Shyann Hane',
+    email: 'Kim_Murphy@gmail.com',
+  },
   {
     id: 26,
     name: 'Ora Breitenberg',
     email: 'Larry_Johnston@gmail.com',
   },
-  { id: 27, name: "Hillary D'Amore", email: 'Emelia50@hotmail.com' },
+  {
+    id: 27,
+    name: "Hillary D'Amore",
+    email: 'Emelia50@hotmail.com',
+  },
   {
     id: 28,
     name: 'Sidney Hanesak DVM',
