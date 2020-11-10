fff3c259edf7c53ec7e021921602f4c49ad7862a
MD. Sakib Khan <programmingwithsakib@gmail.com>
2020-11-10 12:24:42 +0600

    Rest Api Assignment Starter File Added

---
+++ Assignment Hints & Solution/Rest Api/restApiStarter.js
@@ -0,0 +1,51 @@
+const bicycles = [
+  {
+    id: '0',
+    name: 'Hero Spunky 26TSS',
+    hasDiscount: true,        
+    discount: 25,
+    originalPrice: 4000,      
+    star: 4
+  },
+  {
+    id: '1',
+    name: 'Hero Skyper 26T',
+    hasDiscount: true,
+    discount: 30,
+    originalPrice: 2000,
+    star: 5
+  },
+  {
+    id: '2',
+    name: 'Hercules Roadeo A50',
+    hasDiscount: true,
+    discount: 40,
+    originalPrice: 1700,
+    star: 3
+  },
+  {
+    id: '3',
+    name: 'Hero Stomper 16',
+    hasDiscount: false,
+    originalPrice: 1950,
+    star: 4
+  },
+  {
+    id: '4',
+    name: 'WaltX Trail 27.5 T',
+    hasDiscount: true,
+    discount: 25,
+    originalPrice: 2000,
+    star: 3
+  },
+  {
+    id: '5',
+    name: 'Urban Terrain UT1000',
+    hasDiscount: false,
+    originalPrice: 1500,
+    star: 5
+  }
+]
+
+// Start writing codes from here 
+// Best of luck
